2677c6bd07712e50f3fc7c8284864f90d974385d
Andrew Huth <git@huth.me>
2024-09-28 22:36:04 -0400

    Add remaining animations

--- src/components/Cat.tsx
+++ src/components/Cat.tsx
@@ -2,7 +2,17 @@ import clsx from 'clsx';
 import styles from './Cat.module.css';
 
 type Props = {
-  action: 'sitting' | 'sleeping';
+  action:
+    | 'runningDown'
+    | 'runningDownAndLeft'
+    | 'runningDownAndRight'
+    | 'runningLeft'
+    | 'runningRight'
+    | 'runningUp'
+    | 'runningUpAndLeft'
+    | 'runningUpAndRight'
+    | 'sitting'
+    | 'sleeping';
 };
 
 export default function Cat({action}: Props) {
@@ -10,6 +20,14 @@ export default function Cat({action}: Props) {
     <div
       className={clsx(
         "h-8 w-8 bg-[url('/oneko-classic.gif')]",
+        action === 'runningDown' && styles.runningDown,
+        action === 'runningDownAndLeft' && styles.runningDownAndLeft,
+        action === 'runningDownAndRight' && styles.runningDownAndRight,
+        action === 'runningLeft' && styles.runningLeft,
+        action === 'runningRight' && styles.runningRight,
+        action === 'runningUp' && styles.runningUp,
+        action === 'runningUpAndLeft' && styles.runningUpAndLeft,
+        action === 'runningUpAndRight' && styles.runningUpAndRight,
         action === 'sitting' && styles.sitting,
         action === 'sleeping' && styles.sleeping,
       )}
