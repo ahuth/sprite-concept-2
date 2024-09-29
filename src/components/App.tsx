5269e57109a5dfb461395abf39ad99cc0363e26a
Andrew Huth <git@huth.me>
2024-09-28 21:53:11 -0400

    Extract a Cat component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
+import Cat from './Cat';
+
 export default function App() {
-  return (
-    <div>
-      <div className="h-8 w-8 bg-[url('/oneko-classic.gif')] bg-[-64px_0]" />
-    </div>
-  );
+  return <Cat />;
 }
---
+++ src/components/Cat.tsx
@@ -0,0 +1,5 @@
+export default function Cat() {
+  return (
+    <div className="h-8 w-8 bg-[url('/oneko-classic.gif')] bg-[-64px_0]" />
+  );
+}
